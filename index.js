1aed17f580a8185b349f446bd51484c48aa779c4
anastafish <anastafish24@gmail.com>
2023-05-13 16:56:22 +0300

    done!

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
